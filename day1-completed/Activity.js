ffebaa41ea60a2fe8a514d3f0eef74c2fe6cdf19
IronicDeGawd <adityasrivastava807@gmail.com>
2024-07-15 23:15:39 +0530

    Day 3 completed

--- day1-completed/Activity.js
+++ day1-completed/Activity.js
@@ -42,3 +42,28 @@ constant = 10;
 } finally{
     console.log("Constant cannot be changed");
 }
+
+//Feature Request
+//1.
+function typePrint(){
+    console.table({no:typeof(no), str:typeof(str), bool:typeof(bool), obj:typeof(obj), arr:typeof(arr)});
+}
+typePrint();
+
+
+//2.
+function assignment(){
+    let a = 10;
+    console.log("a : ", a);
+    const b = 10;
+    try{
+        a=20;
+        b=20
+    }catch(e){
+        console.log(e);
+    }finally{
+        console.log("a : ", a);
+        console.log("b : ", b);
+    }
+}
+assignment();
