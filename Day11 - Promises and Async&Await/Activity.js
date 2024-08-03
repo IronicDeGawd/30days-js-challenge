c04e06e2f756de23450dc7f63a7dece62607f3dd
IronicDeGawd <adityasrivastava807@gmail.com>
2024-08-03 19:55:56 +0530

    Day 11 completed

--- Day11 - Promises and Async&Await/Activity.js
+++ Day11 - Promises and Async&Await/Activity.js
@@ -51,40 +51,103 @@ function Activity2(){
 
 //Activity 3
 
-const promiseThree = new Promise((resolve,reject)=>{
-   setTimeout(()=>{
-    console.log(`Promise Execution started`);
-    resolve(`Resolved in 2 sec`);
-    // reject();
-   },2000)
-});
-
-async function awaitUse(){
-    console.log(`Calling promise...`);
-    const result = await promiseThree;
-    console.log(result);
+function Activity3(){
+    const promiseThree = new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+         console.log(`Promise Execution started`);
+         resolve(`Resolved in 2 sec`);
+         // reject();
+        },2000)
+     });
+     
+     async function awaitUse(){
+         console.log(`Calling promise...`);
+         const result = await promiseThree;
+         console.log(result);
+     }
+     
+     // awaitUse();
+     
+     const promiseFour = new Promise((resolve,reject)=>{
+         setTimeout(()=>{
+             reject(`Error`)
+         },2000)
+     })
+     
+     async function tryCatch()
+         {
+             try {
+                 console.log(`Calling promise...`);
+                 const result = await promiseFour;
+                 console.log(result);
+             } catch (error) {
+                 console.log(error);
+             }
+         };
 }
-
-// awaitUse();
-
-const promiseFour = new Promise((resolve,reject)=>{
-    setTimeout(()=>{
-        reject(`Error`)
-    },2000)
-})
-
-async function tryCatch()
-    {
-        try {
-            console.log(`Calling promise...`);
-            const result = await promiseFour;
-            console.log(result);
-        } catch (error) {
-            console.log(error);
-        }
-    };
 
 // tryCatch();
 
 //Activity 4
+const url = "https://www.themealdb.com/api/json/v1/1/list.php?c=list"
 
+function activity4(){
+    fetch(url, {method: "GET"})
+.then((response)=>{
+    return response.json()
+})
+.then((data)=>{
+    console.log(data);
+})
+.catch((err)=> console.log(err))
+.finally(()=> console.log(`Execution complete`));
+
+async function mealDb() {
+    try {
+        const response = await fetch(url);
+        const data = await response.json()
+        console.log(data);
+        
+    } catch (error) {
+        console.log(`Fetch error: ${error}`);
+    }
+    
+}
+mealDb();
+}
+//activity4();
+
+//Activity 5
+
+const promiseFirst = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        console.log(`First Promise Executed`);
+        resolve('one');
+    },1000)
+})
+const promiseSec = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        console.log(`Second Promise Executed`);
+        resolve('two');
+    },2000)
+})
+const promiseThird = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        console.log(`Third Promise Executed`);
+        resolve('three');
+    },3000)
+})
+// const promiseFourth = new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//         console.log(`Fourth Promise Executed`);
+//         reject();
+//     },400)
+// })
+
+Promise.race([promiseFirst,promiseSec,promiseThird]).then((values)=>{
+    console.log(`Output for Promise.race: ${values}`);
+})
+
+Promise.all([promiseFirst,promiseSec,promiseThird]).then((values)=>{
+    console.log(`Output for Promise.all: ${values}`);
+})
